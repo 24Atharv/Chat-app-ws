6d99ac02a39ac26a19c3266aa96ccc08b22bffcb
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-07-26 23:02:38 +0530

    Join a room ep

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({port: 8080});
+
+interface User {
+    socket: WebSocket,
+    room: string
+}
+
+let allSocket: User[] = []
 
 wss.on("connection", (socket) => {
     socket.on("message", (message) => {
-        console.log(message.toString());
+        let pareseMessage = JSON.parse(message as unknown as string);
+
+        if(pareseMessage.type === 'join') {
+            allSocket.push({
+                socket: socket,
+                room: pareseMessage.payload.roomId
+            })
+        }
     })
 })
 
