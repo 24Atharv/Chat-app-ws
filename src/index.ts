1a1d1615e46a40433edb80363bb9b131f771f194
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-07-26 23:24:05 +0530

    send a message ep

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { WebSocketServer, WebSocket } from "ws";
-const wss = new WebSocketServer({port: 8080});
+const wss = new WebSocketServer({ port: 8080 });
 
 interface User {
     socket: WebSocket,
@@ -9,14 +9,30 @@ interface User {
 let allSocket: User[] = []
 
 wss.on("connection", (socket) => {
-    socket.on("message", (message) => {
-        let pareseMessage = JSON.parse(message as unknown as string);
+    socket.on('message', (message) => {
+        let parsedMessage = JSON.parse(message as unknown as string);
 
-        if(pareseMessage.type === 'join') {
+        if (parsedMessage.type === 'join') {
+            console.log("User joined room " + parsedMessage.payload.roomId);
             allSocket.push({
-                socket: socket,
-                room: pareseMessage.payload.roomId
+                socket,
+                room: parsedMessage.payload.roomId
             })
+        }
+
+        if (parsedMessage.type === 'chat') {
+            console.log("user wants to chat");
+            let currentUserRoom = null;
+            let found = allSocket.find((x) => x.socket = socket);
+            if (found) {
+                currentUserRoom = found.room
+            }
+
+            for (let i = 0; i < allSocket.length; i++) {
+                if (allSocket[i].room == currentUserRoom) {
+                    allSocket[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     })
 })
