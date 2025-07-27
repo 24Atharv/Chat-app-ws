18de7a8956e36bf7159d0e88c801b469bf96e41d
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-07-27 08:46:08 +0530

    Roomecode Added

--- chat-app-fe/src/App.tsx
+++ chat-app-fe/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { NavBar } from './components/navbar'
+import { RoomCode } from './components/RoomCode'
 
 function App() {
   return <div className='bg-black min-h-screen flex py-10 justify-center text-white'>
-    <div className='border border-[#a3a3a3] p-6 h-auto w-auto rounded-2xl'>
+    <div className='border border-[#a3a3a3] p-6 h-auto w-102 rounded-2xl'>
     <NavBar />
+    <RoomCode />
     </div>
   </div>
 }
