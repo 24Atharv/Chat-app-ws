4535f8f38f629f8ba91cdcf53eba310a04f498cb
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-07-27 08:35:14 +0530

    navabar component added

--- chat-app-fe/src/App.tsx
+++ chat-app-fe/src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
+import { NavBar } from './components/navbar'
 
 function App() {
-  return <div>
-    
+  return <div className='bg-black min-h-screen flex py-10 justify-center text-white'>
+    <div className='border border-[#a3a3a3] p-6 h-auto w-auto rounded-2xl'>
+    <NavBar />
+    </div>
   </div>
 }
 
