9a59b37f539f5357a7080be0f4dbb108a0a6f200
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-07-27 09:01:59 +0530

    fe component added

--- chat-app-fe/src/App.tsx
+++ chat-app-fe/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
+import { Footer } from './components/Footer'
 import { NavBar } from './components/navbar'
 import { RoomCode } from './components/RoomCode'
+import { ShowBar } from './components/ShowBar'
 
 function App() {
   return <div className='bg-black min-h-screen flex py-10 justify-center text-white'>
-    <div className='border border-[#a3a3a3] p-6 h-auto w-102 rounded-2xl'>
+    <div className='border border-[#a3a3a3] p-6 h-auto w-120 rounded-xl'>
     <NavBar />
     <RoomCode />
+    <ShowBar />
+    <Footer />
     </div>
   </div>
 }
---
+++ chat-app-fe/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return <div className="py-3 px-3 border border-[#404040] rounded mt-3
+    flex justify-between">
+        <input type="text" placeholder="Type a message..." />
+        <span className="bg-gray-200 text-black px-3 py-1 rounded">
+            <button>Send</button>
+        </span>
+    </div>
+}
+
