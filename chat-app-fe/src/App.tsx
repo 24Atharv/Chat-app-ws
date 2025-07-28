e37463beb1fe2b6f5d63039348c962da0d9179c8
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-07-28 08:08:48 +0530

    dashboard added

--- chat-app-fe/src/App.tsx
+++ chat-app-fe/src/App.tsx
@@ -1,18 +1,9 @@
 import './App.css'
-import { Footer } from './components/Footer'
-import { NavBar } from './components/navbar'
-import { NewRoom } from './components/NewRoom'
-import { RoomCode } from './components/RoomCode'
-import { ShowBar } from './components/ShowBar'
+import { Dashboard } from './components/Dashboard'
 
 function App() {
-  return <div className='bg-black min-h-screen flex py-10 justify-center text-white'>
-    <div className='border border-[#a3a3a3] p-6 h-auto w-120 rounded-xl'>
-    <NavBar />
-    <RoomCode />
-    <ShowBar />
-    <Footer />
-    </div>
+  return <div>
+    <Dashboard />
   </div>
 }
 
--- chat-app-fe/src/components/Footer.tsx
+++ chat-app-fe/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return <div className="py-3 px-3 border border-[#404040] rounded mt-3
     flex justify-between">
-        <input type="text" placeholder="Type a message..." />
+        <input id="message" type="text" placeholder="Type a message..." />
         <span className="bg-gray-200 text-black px-3 py-1 rounded">
             <button>Send</button>
         </span>
